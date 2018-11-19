1db580188668aaef9674adc2d958babefca48bbf
kthomas852 <kthomas852@yahoo.com>
2018-11-19 15:29:28 -0700

    rolling

--- app/routes.js
+++ app/routes.js
@@ -1,42 +1,12 @@
 var User = require('./models/user');
 module.exports = function(app, passport){
-	app.get('/', function(req, res){
-		res.render('index.ejs');
+	app.get('/', isLoggedIn, function(req, res){
+		res.render('home.ejs');
 	});
 
 	app.get('/home', function(req, res){
 		res.render('home.ejs');
 	});
-
-	app.get('/login', function(req, res){
-		res.render('login.ejs', { message: req.flash('loginMessage') });
-	});
-	app.post('/login', passport.authenticate('local-login', {
-		successRedirect: '/profile',
-		failureRedirect: '/login',
-		failureFlash: true
-	}));
-
-	app.get('/signup', function(req, res){
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
-	});
-
-
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect: '/',
-		failureRedirect: '/signup',
-		failureFlash: true
-	}));
-
-	app.get('/profile', isLoggedIn, function(req, res){
-		res.render('profile.ejs', { user: req.user });
-	});
-
-	app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
-
-	app.get('/auth/facebook/callback', 
-	  passport.authenticate('facebook', { successRedirect: '/profile',
-	                                      failureRedirect: '/' }));
 
 	app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
@@ -53,8 +23,8 @@ module.exports = function(app, passport){
 
 function isLoggedIn(req, res, next) {
 	if(req.isAuthenticated()){
-		return next();
+		next();
+	}else{
+		res.redirect('/auth/google');
 	}
-
-	res.redirect('/login');
 }
