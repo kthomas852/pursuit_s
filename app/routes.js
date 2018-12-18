76aa847aa5cca677e76b6c4f3b894e6271d2b236
Kyle Thomas <kthomas852@yahoo.com>
2018-12-18 10:24:48 -0700

    Site persistance finished

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,86 @@ module.exports = function(app, passport){
 		res.render('home.ejs');
 	});
 
+	app.get('/submit', function(req, res){
+		res.render('submit.ejs');
+	});
+
+	app.get('/submit/openmic', function(req, res){
+		res.render('submit/open_mic.ejs');
+	});
+
+	app.get('/submit/fill_in_friday', function(req, res){
+		res.render('submit/fill_in_friday.ejs');
+	});
+
+	app.get('/submit/on_shelf', function(req, res){
+		res.render('submit/on_shelf.ejs');
+	});
+
+	app.get('/submit/industry_info', function(req, res){
+		res.render('submit/industry_info.ejs');
+	});
+
+	app.get('/submit/kitch', function(req, res){
+		res.render('submit/kitch.ejs');
+	});
+
+	app.get('/submit/fashion', function(req, res){
+		res.render('submit/fashion.ejs');
+	});
+
+	app.get('/submit/education_events', function(req, res){
+		res.render('submit/education_events.ejs');
+	});
+
+	app.get('/submit/training', function(req, res){
+		res.render('submit/training.ejs');
+	});
+
+	app.get('/submit/general', function(req, res){
+		res.render('submit/general.ejs');
+	});
+
+	app.get('/submit/ted', function(req, res){
+		res.render('submit/TED.ejs');
+	});
+
+	app.get('/submit/theme_days', function(req, res){
+		res.render('submit/theme_days.ejs');
+	});
+
+	app.get('/submit/wins_personal', function(req, res){
+		res.render('submit/wins_personal.ejs');
+	});
+
+	app.get('/submit/events', function(req, res){
+		res.render('submit/events.ejs');
+	});
+
+	app.get('/submit/groups', function(req, res){
+		res.render('submit/groups.ejs');
+	});
+
+	app.get('/submit/wins_personal', function(req, res){
+		res.render('submit/wins_personal.ejs');
+	});
+
+	app.get('/submit/wins_team', function(req, res){
+		res.render('submit/wins_team.ejs');
+	});
+
+	app.get('/submit/mvp', function(req, res){
+		res.render('submit/mvp.ejs');
+	});
+
+	app.get('/submit/yelp_works', function(req, res){
+		res.render('submit/yelp_works.ejs');
+	});
+
+	app.get('/submit/seize_mic', function(req, res){
+		res.render('submit/seize_mic.ejs');
+	});
+
 	app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
 
 	app.get('/auth/google/callback', 
